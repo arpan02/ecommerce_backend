9f17c795df37ee7c4df05f61ac9d90713f5c921a
Arpan Pathak <arpanpathak02@gmail.com>
2023-02-07 19:44:35 +0530

    first commit

---
+++ config/environment.js
@@ -0,0 +1,19 @@
+/*****************************************
+ * ENVIRONMENT
+ ******************************************/
+
+const development = {
+  name: 'development',
+  db: process.env.MONGO_URL,
+  port: 8080,
+  jwt_secret: 'secret',
+};
+
+const production = {
+  port: 8080,
+  name: 'production',
+  db: process.env.MONGO_UR,
+  jwt_secret: process.env.JWT_SECRET,
+};
+
+module.exports = process.env.NODE_ENV === undefined ? development : production;
